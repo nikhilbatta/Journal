6bb6bfdd1e93e16a496edaffad520e6c14baabd2
kira roberts and nikhil batta <kira@>
2019-09-03 16:19:18 -0700

    adds simple display

--- src/journal1.js
+++ src/journal1.js
@@ -1,27 +1,32 @@
+import $ from 'jquery';
+//backEnd
 export function Journal() {
-  this.entries = []
+  this.entries = [];
 }
-
 Journal.prototype.addEntry = function(entry) {
   this.entries.push(entry);
 }
-
 export function Entry(title, entry) {
   this.title = title,
   this.entry = entry
 }
-
 Entry.prototype.numberOfWords = function() {
   var wordCounter = this.entry.trim().split(/\s+/).length;
-  console.log(wordCounter)
+  return wordCounter;
 }
 Entry.prototype.allCharacters = function(){
   var characterCounter = this.entry.split("").length;
-  console.log(characterCounter);
+  return characterCounter;
 }
 Entry.prototype.allVowels = function(){
   var vowelCounter = this.entry.match(/[aeiou]/g);
-  
-  // var vowelCounter = this.entry.split("a","e","i","o","u",0).length;
-  console.log(vowelCounter)
+  return vowelCounter;
+}
+// frontEnd
+Journal.prototype.display = function(){
+  this.entries.forEach(function(entry1){
+      $("#listOfStuff").append("<li>" + "The number of words are: " + " "  + entry1.numberOfWords() + "</li>");
+      $("#listOfStuff").append("<li>" + "The number of characters are: " + " "  + entry1.allCharacters() + "</li>");
+      $("#listOfStuff").append("<li>" + "The number of vowels are: " + " "  + entry1.allVowels() + "</li>");
+  });
 }
--- src/main.js
+++ src/main.js
@@ -5,17 +5,24 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
+var newJournal = new Journal();
 $(document).ready(function() {
-  var newJournal = new Journal();
   $("form#journal").submit(function(event) {
     event.preventDefault();
     var inputTitle = $("input#title").val();
     var inputEntry = $("input#entry").val();
     var newEntry = new Entry(inputTitle, inputEntry);
-    newEntry.numberOfWords();
-    newEntry.allCharacters();
-    newEntry.allVowels();
     newJournal.addEntry(newEntry);
+    $("#outputDisplay").text(inputTitle);
+    $("#outputDisplay1").text(inputEntry);
+    $("#listOfStuff").show();
+    newJournal.display();
     console.log(newJournal);
+  });
+  $("#newWack").click(function(){
+    newJournal = new Journal();
+    $("#outputDisplay").text(" ");
+    $("#outputDisplay1").text(" ");
+    $("#listOfStuff").hide();
   });
 });
